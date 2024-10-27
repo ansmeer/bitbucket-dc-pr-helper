7d67f788c993399a542d24976a86fe87af06cfc2
Luisa <luisa.mohle@no.experis.com>
2024-10-27 19:34:31 +0100

    Improve styling based on PR status

--- src/overview.ts
+++ src/overview.ts
@@ -16,7 +16,7 @@ prStore.subscribe(
 export const addOrUpdatePrOverview = (pullRequest: PullRequest) => {
 	const reviewedFiles = pullRequest.files.filter((f) => f.reviewed)
 	const statusPercentage = Math.round((reviewedFiles.length / pullRequest.files.length) * 100)
-
+	const isPrCompleted = reviewedFiles.length === pullRequest.files.length
 	const changesToolbar = document.querySelector("div.changes-scope-toolbar")
 	if (!changesToolbar) {
 		throw new Error("Could not find changes toolbar")
@@ -24,13 +24,15 @@ export const addOrUpdatePrOverview = (pullRequest: PullRequest) => {
 
 	const overviewId = "bb-overview"
 	const textContent = `Reviewed ${reviewedFiles.length} of ${pullRequest.files.length} files (${statusPercentage}%)`
-	const overviewDiv = document.querySelector(`#${overviewId}`)
-	if (overviewDiv) {
-		overviewDiv.textContent = textContent
+	const existingOverviewElement = document.querySelector(`#${overviewId}`)
+	if (existingOverviewElement) {
+		existingOverviewElement.textContent = textContent
+		existingOverviewElement.className = isPrCompleted ? "complete" : "incomplete"
 	} else {
-		const overviewParent = document.createElement("div")
-		overviewParent.id = overviewId
-		overviewParent.textContent = `reviewed ${reviewedFiles.length} of ${pullRequest.files.length} files (${statusPercentage}%)`
-		changesToolbar.insertBefore(overviewParent, changesToolbar.firstChild)
+		const overviewElement = document.createElement("div")
+		overviewElement.id = overviewId
+		overviewElement.className = isPrCompleted ? "complete" : "incomplete"
+		overviewElement.textContent = `reviewed ${reviewedFiles.length} of ${pullRequest.files.length} files (${statusPercentage}%)`
+		changesToolbar.insertBefore(overviewElement, changesToolbar.firstChild)
 	}
 }
